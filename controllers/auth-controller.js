c36e845065c9932778d98f375bda2746d9e4f892
Sohag-84 <ihsohag84@gmail.com>
2025-08-11 18:41:21 +0600

    user login with token api created

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -55,4 +55,54 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = { registerController };
+//login controller
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //check user exits or not in the database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: `User doesn't exists`,
+      });
+    }
+    //check password correct or not
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    //create user token
+    const accessToken = await jwt.sign(
+      {
+        userId: user._id,
+        name: user.username,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfull",
+      token: accessToken,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
+
+module.exports = { registerController, loginController };
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-
 const connectToDB = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
+const { loginController } = require("./controllers/auth-controller");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/auth",authRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("api/auth", loginController);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
